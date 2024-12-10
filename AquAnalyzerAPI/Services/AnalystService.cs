184bbbc5b79c5db31f0091a1b324956e7d29860c
M316354 <316354@viauc.dk>
2024-12-10 09:59:30 +0100

    adding missing 'using' statements in services

--- AquAnalyzerAPI/Services/AnalystService.cs
+++ AquAnalyzerAPI/Services/AnalystService.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
-
+using AquAnalyzerAPI.Files;
+using AquAnalyzerAPI.Interfaces;
 
 namespace AquAnalyzerAPI.Services
 {
--- AquAnalyzerAPI/Services/VisualDesignerService.cs
+++ AquAnalyzerAPI/Services/VisualDesignerService.cs
@@ -1,4 +1,6 @@
 using Microsoft.EntityFrameworkCore;
+using AquAnalyzerAPI.Files;
+using AquAnalyzerAPI.Interfaces;
 
 namespace AquAnalyzerAPI.Services
 {
