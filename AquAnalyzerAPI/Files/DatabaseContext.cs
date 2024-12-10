eae7643108b9cda772f696e17c27da3dd3dfdfc3
Bautist1000 <91956082+Bautist1000@users.noreply.github.com>
2024-12-10 13:29:29 +0100

    Refactor DatabaseContext to reorder DbSet properties; establish many-to-many relationships between WaterMetrics, WaterData, and Visualisations

--- AquAnalyzerAPI/Files/DatabaseContext.cs
+++ AquAnalyzerAPI/Files/DatabaseContext.cs
@@ -8,18 +8,15 @@ namespace AquAnalyzerAPI.Files
         public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
         {
         }
-
-        public DbSet<WaterMetrics> WaterMetrics { get; set; }
         public DbSet<Abnormality> Abnormalities { get; set; }
-        public DbSet<WaterData> WaterData { get; set; }
         public DbSet<Notification> Notifications { get; set; }
         public DbSet<Analyst> Analysts { get; set; }
         public DbSet<VisualDesigner> VisualDesigners { get; set; }
         public DbSet<Report> Reports { get; set; }
         public DbSet<User> Users { get; set; }
-
         public DbSet<Visualisation> Visualisations { get; set; }
-
+        public DbSet<WaterData> WaterData { get; set; }
+        public DbSet<WaterMetrics> WaterMetrics { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
@@ -36,10 +33,23 @@ namespace AquAnalyzerAPI.Files
             modelBuilder.Entity<User>().UseTpcMappingStrategy();
             modelBuilder.Entity<Analyst>().ToTable("Analyst").Property(s => s.Id).ValueGeneratedOnAdd();
             modelBuilder.Entity<VisualDesigner>().ToTable("VisualDesigner").Property(s => s.Id).ValueGeneratedOnAdd();
-            modelBuilder.Entity<Bachelor>().ToTable("Bachelors").Property(b => b.Id).ValueGeneratedOnAdd();
 
 
+            modelBuilder.Entity<WaterMetrics>()
+                       .HasMany(w => w.Visualisations)
+                       .WithMany(v => v.MetricsUsed)
+                       .UsingEntity(
+                           "WaterMetricsVisualisation",
+                           l => l.HasOne(typeof(Visualisation)).WithMany().HasForeignKey("VisualisationId"),
+                           r => r.HasOne(typeof(WaterMetrics)).WithMany().HasForeignKey("WaterMetricsId"));
 
+            modelBuilder.Entity<WaterData>()
+                    .HasMany(w => w.Visualisations)
+                    .WithMany(v => v.RawDataUsed)
+                    .UsingEntity(
+                         "WaterDataVisualisation",
+                         l => l.HasOne(typeof(Visualisation)).WithMany().HasForeignKey("VisualisationId"),
+                        r => r.HasOne(typeof(WaterData)).WithMany().HasForeignKey("WaterDataId"));
         }
     }
 }
