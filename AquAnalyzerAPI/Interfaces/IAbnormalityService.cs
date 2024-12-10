fd6625d2bd2cc8ccbd1c4911a12144ac8b9a38c8
Bautist1000 <91956082+Bautist1000@users.noreply.github.com>
2024-12-10 10:59:14 +0100

    Add IAbnormalityService interface with CRUD method definitions

---
+++ AquAnalyzerAPI/Interfaces/IAbnormalityService.cs
@@ -0,0 +1,12 @@
+namespace AquAnalyzerAPI.Interfaces
+{
+    public interface IAbnormalityService
+    {
+        Task<Abnormality> AddAbnormality(Abnormality abnormality);
+        Task<IEnumerable<Abnormality>> GetAllAbnormalities();
+        Task<Abnormality> GetAbnormalityById(int id);
+        Task<IEnumerable<Abnormality>> GetAbnormalitiesByType(string type);
+        Task<bool> UpdateAbnormality(int id, string description, string type);
+        Task<bool> DeleteAbnormality(int id);
+    }
+}
