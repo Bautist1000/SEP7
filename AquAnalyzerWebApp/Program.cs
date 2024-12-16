b200b391f5cae61ac3d28ac18dfec27616772b42
Bautist1000 <91956082+Bautist1000@users.noreply.github.com>
2024-12-16 20:40:26 +0100

    Remove redundant service registrations for IAbnormalityService and INotificationService

--- AquAnalyzerWebApp/Program.cs
+++ AquAnalyzerWebApp/Program.cs
@@ -15,8 +15,7 @@ builder.Services.AddRazorComponents()
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5126/") });
 builder.Services.AddScoped<INotificationsService, NotificationsService>();
-builder.Services.AddScoped<IAbnormalityService, AbnormalityService>();
-builder.Services.AddScoped<INotificationService, NotificationService>();
+
 
 builder.Services.AddDbContext<DatabaseContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
