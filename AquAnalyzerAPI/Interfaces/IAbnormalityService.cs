31d174630bd5bf1d07c150e4daa2b15fadffcb74
Alejandro Bautista Martin <91956082+Bautist1000@users.noreply.github.com>
2024-12-20 01:50:18 +0100

    Update IAbnormalityService.cs

--- AquAnalyzerAPI/Interfaces/IAbnormalityService.cs
+++ AquAnalyzerAPI/Interfaces/IAbnormalityService.cs
@@ -9,10 +9,12 @@ namespace AquAnalyzerAPI.Interfaces
         Task<IEnumerable<Abnormality>> GetAllAbnormalities();
         Task<Abnormality> GetAbnormalityById(int id);
         Task<IEnumerable<Abnormality>> GetAbnormalitiesByType(string type);
-        Task<bool> MarkAsDealtWith(int id);
+        Task<bool> MarkAbnormalityAsDealtWith(int abnormalityId);
         Task<bool> UpdateAbnormality(int id, string description, string type);
         Task<bool> DeleteAbnormality(int id);
         Task<IEnumerable<Abnormality>> CheckWaterDataAbnormalities(int dataId);
         Task<IEnumerable<Abnormality>> CheckWaterMetricsAbnormalities(int metricsId);
+        Task RemoveDuplicateAbnormalities(int waterDataId);
+
     }
 }
